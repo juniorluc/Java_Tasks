9b97a32bfa47177ca321929765a2629e267a3c2c
Junior Medeiros <juniormedeirosluce@gmail.com>
2022-11-18 17:39:32 -0300

    Atividade 01: Escreva um programa em Java que exiba o seu nome completo na tela do computador.

--- src/Q1/ExibeNome.java
+++ src/Q1/ExibeNome.java
@@ -1,5 +1,11 @@
 package Q1;
 
 public class ExibeNome {
+    public static void main(String[] args) {
 
+        String nome = "Marcos Andrade Souto";
+
+        System.out.println(nome);
+
+    }
 }
